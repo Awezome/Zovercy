2afa15657cc2ac9f103b4bd6eda651ed939776f2
Awesomez <zypypz@qq.com>
2013-11-08 13:11:41 +0800

    some clean

--- Config.php
+++ Config.php
@@ -3,9 +3,9 @@
 $CONFIG['DB'] = array(
     'TYPE' => 'mysql', //数据库类型
     'HOST' => '127.0.0.1', //数据库地址
-    'NAME' => 'weblook', //数据库名
-    'USER' => 'root', //数据库用户名
-    'PWD' => '387420489', // 数据库密码
+    'NAME' => '', //数据库名
+    'USER' => '', //数据库用户名
+    'PWD' => '', // 数据库密码
     'TABLEPRE' => 'hisunphp_', // 表前缀
     'CHARSET' => 'utf8', // MySQL 字符集, 可选 'gbk', 'big5', 'utf8', 'latin1', 留空为按照默认字符集设定
 );
