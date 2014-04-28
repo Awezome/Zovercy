5d77f5b708a3b71ec88f266962a907fe00b88e9f
Awesomez <likezyp@gmail.com>
2014-04-28 22:37:57 +0800

    added posttype edit, update, delete

--- app/admin/controller/posttype.php
+++ app/admin/controller/posttype.php
@@ -23,24 +23,58 @@ class posttype extends Controller{
     }
 
     function save(){
+        $id=Input::number('id');
         $name=Input::text('typename');
         $parentid=Input::number('posttype');
-
-        DB::table('posttype')->save(array(
-            'cname'=>$name,
-            'parentid'=>$parentid,
-            'level'=>$this->model->getLevel($parentid),
-        ));
-
+        if($id>0){
+            DB::table('posttype')->where('ptid=?',array($id))->update(array(
+                'cname'=>$name,
+                'parentid'=>$parentid,
+            ));
+        }else{
+            DB::table('posttype')->save(array(
+                'cname'=>$name,
+                'parentid'=>$parentid,
+                'level'=>$this->model->getLevel($parentid),
+            ));
+        }
         Html::jump(URL::controller());
     }
 
     function edit(){
+        $id=Get::number(0);
+        if($id>0){
+            $results=DB::table('posttype')->where('ptid=?',array($id))->find('cname,parentid');
+            $parentid=$results['parentid'];
+            $typename=$results['cname'];
+        }else{
+            $parentid=0;
+            $typename='';
+        }
         $ddd=$this->model->getPosttype();
         $data=array(
-            'newDataSelect'=>$this->model->showPostSelect($ddd),
+            'name'=>$typename,
+            'id'=>$id,
+            'newDataSelect'=>$this->model->showPostSelect($ddd,$parentid),
         );
         $this->setData($data);
         $this->loadPage('posttypenew');
+    }
+
+    function delete(){
+        $id=Get::number(0);
+        if($id>0){
+            $del=DB::transaction(function() use ($id){
+                DB::table('posttype')->where('parentid=?',array($id))->update(array(
+                    'parentid'=>0,
+                    'level'=>1,
+                ));
+                return DB::table('posttype')->where('ptid=?',array($id))->delete();
+            });
+
+            if($del>0){
+                Html::jump(URL::controller());
+            }
+        }
     }
 }
