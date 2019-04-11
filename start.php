71182feb78c83c6a662914f64e780309b1465d3a
Awezome <likezyp@gmail.com>
2019-04-11 15:27:33 +0800

    add http status code

--- start.php
+++ start.php
@@ -6,6 +6,7 @@
  */
 require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
+use Workerman\Protocols\Http;
 
 Class App{
     private $worker=null;
@@ -14,13 +15,22 @@ Class App{
     function __construct()
     {
         $this->worker=new Worker("http://0.0.0.0:2345");
-        $this->worker->count=1;
+        $this->worker->count=2;
         $this->worker->onWorkerStart=function (){
             $this->onWorkerStart();
         };
         $this->worker->onMessage = function($connection, $data)
         {
-            $this->onMessage($connection, $data);
+            try{
+                $data=$this->onMessage($data);
+                $code=200;
+            }catch (\Exception $e){
+                $data=$e->getMessage();
+                $code=500;
+            };
+
+            Http::header("Content-Type: application/json;charset=utf-8",true,$code);
+            $connection->send($data);
         };
     }
 
@@ -29,11 +39,11 @@ Class App{
         $this->router = new \Zovercy\Http\Router(__DIR__.'/app/Routes/api.php');
     }
 
-    private function onMessage($connection, $data){
+    private function onMessage($data){
         $request=new \Zovercy\Http\Request();
         $response=new \Zovercy\Http\Response();
         $this->router->dispatch($request,$response);
-        $connection->send($response->toJson());
+        return $response->toJson();
     }
 
     function run(){
